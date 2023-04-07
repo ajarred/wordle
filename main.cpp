3257aa6fe7044852bcf202c20d66304b16d9f97c
ajarred <adjarredtan@gmail.com>
2023-04-07 10:27:31 +0800

    Add convert to uppercase function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
+string convertToUppercase(string word) {
+    transform(word.begin(),word.end(),word.begin(),::toupper);
+    return word;
+}
+
 int main()
 {
-    cout << "hello world\n";
+    string userInput;
+    getline(cin,userInput);
+    userInput = convertToUppercase(userInput);
+    cout << userInput;
     return 0;
 }
